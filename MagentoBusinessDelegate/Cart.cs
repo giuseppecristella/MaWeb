2bc2401a269c24cb0f7c3793b9796a5c671162fb
peppe76 <a>
2014-09-26 09:13:38 +0200

    Fix Cart.cs not in source control

---
+++ MagentoBusinessDelegate/Cart.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Ez.Newsletter.MagentoApi;
+
+ 
+namespace MagentoBusinessDelegate
+{
+  public class Cart
+  {
+    private readonly List<Product> _products;
+    private decimal _total;
+       
+    public Cart()
+    {
+      _products = new List<Product>();
+    }
+
+    public IEnumerable<Product> Products
+    {
+      get { return _products; }
+    }
+
+    public decimal Total
+    {
+      get { return _total; }
+    }
+
+    public void AddProductAndUpdateTotal(Product product)
+    {
+      if (!_products.Contains(product))
+      {
+        _products.Add(product);
+      }
+      _total += decimal.Parse(product.price);
+    }
+  }
+}
