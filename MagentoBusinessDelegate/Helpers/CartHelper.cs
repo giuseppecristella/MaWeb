d2502d1f323763047f59f73f2d7048f9a2c40d50
peppe76 <a>
2014-09-26 09:14:46 +0200

    Fix carthelper.cs under source control

---
+++ MagentoBusinessDelegate/Helpers/CartHelper.cs
@@ -0,0 +1,22 @@
+﻿using Ez.Newsletter.MagentoApi;
+using MagentoComunication.Cache;
+
+namespace MagentoBusinessDelegate.Helpers
+{
+  public static class CartHelper
+  {
+    private static ICacheManager _cacheManager;
+     
+    public static ICacheManager CacheManager
+    {
+      set { _cacheManager = value; }
+    }
+
+    public static void AddProductToSessionCart(Product product)
+    {
+      var cart = _cacheManager.Get<Cart>("carrello") ?? new Cart();
+      cart.AddProductAndUpdateTotal(product);
+      _cacheManager.Add("carrello",cart);
+    }
+  }
+}
