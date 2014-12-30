607e58388b56246b2bd3e2b478034fb241a374c4
peppe76 <a>
2014-12-30 14:47:15 +0100

    Revert "-"
    This reverts commit 1cd5fa96c487b0189b77f95cae935aa06fd6261c.

--- MA.Web/Azienda.aspx.cs
+++ MA.Web/Azienda.aspx.cs
@@ -6,6 +6,6 @@ public partial class Azienda : System.Web.UI.Page
   protected void Page_Load(object sender, EventArgs e)
   {
     DataSetVepAdminTableAdapters.AlbumsTableAdapter taNews = new DataSetVepAdminTableAdapters.AlbumsTableAdapter();
-    DataTable dtNews = taNews.GetInfoAlbums(); 
+    DataTable dtNews = taNews.GetInfoAlbums();
   }
 }
