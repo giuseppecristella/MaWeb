af80a0824f960f0c71cf11e098db4c4f38aa9bf7
peppe76 <a>
2015-04-27 17:11:23 +0200

    -

---
+++ MA.Web/Design/Admin/Default.master.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using Ez.Newsletter.MagentoApi;
+public partial class Default : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void _goNewsLetter(object sender, EventArgs e)
+    {
+
+    }
+}
