17bab50f40b303a3a1947fde845c5cc41091d365
giuseppe.cristella <giuseppe.cristella@libero.it>
2015-05-16 21:29:28 +0200

    Redirect fix

--- MA.Web/Default.aspx.cs
+++ MA.Web/Default.aspx.cs
@@ -4,6 +4,6 @@ public partial class Index : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        Response.Redirect("http://shop.materarredamenti.it");
+        Response.Redirect("http://shop.materarredamenti.it/design");
     }
 }
